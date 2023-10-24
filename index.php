cc20b614a20260c2f1d6cd661512185f143f2c85
KimKaYoung <kky00922@gmail.com>
2023-10-24 22:39:24 +0900

    test

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
   error_reporting( E_ALL );
   ini_set( "display_errors", 1 );
-  
+
 include("db.php");
 
 // UTF-8 문자열 자르기
@@ -82,12 +82,6 @@ if($index_result->num_rows> 0){
     $index_options= mysqli_fetch_all($index_result, MYSQLI_ASSOC);
     if(is_array($index_options)) {
         foreach($index_options as $ii => $index_array) {
-            if(!check_index_title($index_array['Field'])) { // 한글로 시작하지 않는 경우 배열에서 삭제함 (영여, 숫자, 한글, 특수문자(_) 허용)
-                unset($index_options[$ii]);
-            }
-            if(in_array($index_array['Field'], array("연도", "분기", "차시", "권역", "기관"))) {   // 연도, 분기, 차시, 권역, 기관 배열에서 삭제함
-                unset($index_options[$ii]);
-            }
         }
     }
     echo "<pre>";
