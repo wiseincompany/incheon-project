2d02f2d7bd783d1d2e6028635e79ba8102ce948a
KimKaYoung <kky00922@gmail.com>
2023-10-24 22:43:30 +0900

    test

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ include("db.php");
 // UTF-8 문자열 자르기
 function php_fn_utf8_to_array($str){
 	$re_arr = array(); $re_icount = 0;
+    /*
 	for($i=0,$m=strlen($str);$i<$m;$i++){
 		$ch = sprintf('%08b',ord($str{$i}));
 		if(strpos($ch,'11110')===0){$re_arr[$re_icount++]=substr($str,$i,4);$i+=3;}
@@ -15,6 +16,7 @@ function php_fn_utf8_to_array($str){
 		else if(strpos($ch,'0')===0){$re_arr[$re_icount++]=substr($str,$i,1);}
 	}
 	return $re_arr;
+    */
 }
 
 //utf8문자열을 잘라낸다.
