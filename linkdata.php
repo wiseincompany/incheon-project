62b718d33f8235d91c78bf24d3e7e938ab59c2e9
KimKaYoung <kky00922@gmail.com>
2023-11-16 20:43:12 +0900

    mod

--- linkdata.php
+++ linkdata.php
@@ -1,3 +1,16 @@
 <?php
-echo "aa";
+include "common.php";
+
+// 데이터 조회
+
+// 있으면 update
+
+// 없으면 insert
+
+// login
+$_SESSION['login_info']['id'] = $uid;
+$_SESSION['login_info']['name'] = $uname;
+
+// 페이지 이동
+header("Location : /index.php");
 ?>
