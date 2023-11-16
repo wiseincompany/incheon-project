7d930878bbf217611e8ccb8e5438eb82f8663a5e
KimKaYoung <kky00922@gmail.com>
2023-11-16 21:22:23 +0900

    mod

--- linkdata.php
+++ linkdata.php
@@ -1,6 +1,9 @@
 <?php
 include "common.php";
 
+$uid = $_REQUEST['uid'];
+$uname = $_REQUEST['uname'];
+
 if($uid == "" || $uname == "") {
     echo "<script>alert('접속 정보가 누락되었습니다.');/*history.go(-1);*/</script>";
     //exit;
