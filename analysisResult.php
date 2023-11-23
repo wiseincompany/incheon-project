c1e74b774b75d1d0ca06f76f1d4b352bc723d547
KimKaYoung <kky00922@gmail.com>
2023-11-24 05:17:57 +0900

    mod

--- analysisResult.php
+++ analysisResult.php
@@ -69,7 +69,7 @@ $result = $conn->query($query);
         }
       };
 
-      var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+      var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
       chart.draw(data, options);
     }
 </script>
